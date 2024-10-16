3725b83630efede40b65e89ed549fe418bc89284
Your Name <you@example.com>
2024-10-16 13:06:43 +0000

    Deeper Client C++

---
+++ out/build/Default/CMakeFiles/deeper_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deeper_client.dir/deeper_client.cpp.o"
+  "CMakeFiles/deeper_client.dir/deeper_client.cpp.o.d"
+  "deeper_client"
+  "deeper_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deeper_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/Default/CMakeFiles/deeper_client.dir/deeper_client.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/deeper_client.dir/deeper_client.cpp.o: \
+ /home/pop-os/deeper_client/deeper_client.cpp /usr/include/stdc-predef.h
